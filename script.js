b27564883214526a8f60c3c93150b2e09bd41a72
mroxso <mail@pascalrost.de>
2023-04-07 11:57:38 +0200

    add styles and pubkey (username)

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ socket.onopen = function(event) {
 };
 
 socket.onmessage = function(event) {
-    const responseContainer = document.getElementById('response-container');
+    const responseContainer = document.getElementById('messages-container');
     const data = JSON.parse(event.data);
     if (data[0] === "EVENT") {
         const content = data[2].content;
+        const pubkey = data[2].pubkey;
+        const pubkeyShortened = `${pubkey.slice(0, 3)}...${pubkey.slice(-3)}`;
         const para = document.createElement('p');
-        para.innerHTML = content;
+        para.innerHTML = pubkeyShortened + ": " + content;
         responseContainer.insertBefore(para, responseContainer.firstChild);
     }
 };
